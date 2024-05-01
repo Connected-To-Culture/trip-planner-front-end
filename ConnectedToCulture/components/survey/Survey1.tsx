6b3546bd5936b25d30a0b2e0d0e50ae0ebefae46
Kim Love <kimberlymlove15@gmail.com>
2024-05-01 11:42:45 -0400

    Created initial, basic Survey1 componenet

---
+++ ConnectedToCulture/components/survey/Survey1.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Survey1 = () => {
+  return (
+    <View>
+      <Text>Survey1</Text>
+    </View>
+  );
+};
+
+export default Survey1;
