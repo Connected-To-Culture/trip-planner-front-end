ada882a7029b9d0011fcf75df7ac1194a31359d1
Kim Love <kimberlymlove15@gmail.com>
2024-05-02 22:11:49 -0400

    Added a View to center content

--- ConnectedToCulture/components/survey/SurveyStart.tsx
+++ ConnectedToCulture/components/survey/SurveyStart.tsx
@@ -13,26 +13,28 @@ const SurveyStart = ({navigation}: SurveyStartProps) => {
 
   return (
     <View style={surveyStyles.container}>
-      <View style={surveyStyles.textContainer}>
-        <Text style={surveyStyles.title}>Welcome to C2C survey!</Text>
-        <Text style={surveyStyles.description}>
-          Make smarter trip plans simply by answering the following 10
-          questions!
-        </Text>
-        <Text style={surveyStyles.estimatedTime}>Estimated time: 3 min</Text>
+      <View style={{alignItems: 'center'}}>
+        <View style={surveyStyles.textContainer}>
+          <Text style={surveyStyles.title}>Welcome to C2C survey!</Text>
+          <Text style={surveyStyles.description}>
+            Make smarter trip plans simply by answering the following 10
+            questions!
+          </Text>
+          <Text style={surveyStyles.estimatedTime}>Estimated time: 3 min</Text>
+        </View>
+        <ResuableButton
+          backgroundColor={COLORS.primary}
+          borderColor={COLORS.primary}
+          borderWidth={1}
+          btnText="Start"
+          onPress={() => navigation.navigate('Survey1')}
+          paddingHorizantal={32}
+          paddingVertical={8}
+          size={16}
+          textColor={COLORS.white}
+          width={187}
+        />
       </View>
-      <ResuableButton
-        backgroundColor={COLORS.primary}
-        borderColor={COLORS.primary}
-        borderWidth={1}
-        btnText="Start"
-        onPress={() => navigation.navigate('Survey1')}
-        paddingHorizantal={32}
-        paddingVertical={8}
-        size={16}
-        textColor={COLORS.white}
-        width={187}
-      />
     </View>
   );
 };
