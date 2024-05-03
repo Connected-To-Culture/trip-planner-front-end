18438af3748ff7fc835cb25411e72358908b61b4
Kim Love <kimberlymlove15@gmail.com>
2024-05-02 23:00:06 -0400

    Added background color to container

--- ConnectedToCulture/components/survey/survey.style.tsx
+++ ConnectedToCulture/components/survey/survey.style.tsx
@@ -5,7 +5,22 @@ import {Dimensions} from 'react-native';
 const {height, width} = Dimensions.get('window');
 
 const surveyStyles = StyleSheet.create({
+  backNextBtnContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+    justifyContent: 'center',
+    marginTop: 100,
+  },
+  backNextBtnContainerLandscape: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+    justifyContent: 'center',
+    marginTop: 70,
+  },
   container: {
+    backgroundColor: COLORS.white,
     flex: 1,
     justifyContent: 'center',
     padding: 20,
@@ -32,10 +47,59 @@ const surveyStyles = StyleSheet.create({
     paddingBottom: 0,
     textAlign: 'center',
   },
+  otherText: {
+    color: COLORS.black,
+    fontFamily: 'Almarai',
+    fontSize: 14,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 19.2,
+    paddingBottom: 0,
+  },
+  question: {
+    color: COLORS.black,
+    fontFamily: 'Almarai',
+    fontSize: 20,
+    fontWeight: '700',
+    letterSpacing: 0,
+    lineHeight: 24,
+    paddingBottom: 0,
+  },
+  specifyOtherContainer: {
+    alignItems: 'center',
+    display: 'flex',
+    gap: 10,
+    flexDirection: 'row',
+    marginTop: 10
+  },
+  subtitle: {
+    color: COLORS.darkGray,
+    fontFamily: 'Almarai',
+    fontSize: 16,
+    fontWeight: '700',
+    letterSpacing: 0,
+    lineHeight: 24,
+    marginBottom: 16,
+    paddingBottom: 0,
+  },
+  surveyBtnsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+    justifyContent: 'center',
+  },
   textContainer: {
     alignItems: 'center',
     marginBottom: 20,
     width: 320
+  },
+  textInput: {
+    backgroundColor: '#CECECE',
+    borderColor: '#CECECE',
+    borderRadius: 10,
+    borderWidth: 1,
+    height: 38,
+    paddingLeft: 10,
   },
   title: {
     color: COLORS.primary,
