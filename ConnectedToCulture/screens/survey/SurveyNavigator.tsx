1942c1dc3c3501eb1002808bb40f02b227e43fd2
Kim Love <kimberlymlove15@gmail.com>
2024-05-02 20:17:58 -0400

    Added SafeAreaView to component

--- ConnectedToCulture/screens/survey/SurveyNavigator.tsx
+++ ConnectedToCulture/screens/survey/SurveyNavigator.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SurveyStart from '../../components/survey/SurveyStart';
 import Survey1 from '../../components/survey/Survey1';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
 const SurveyNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="SurveyStart" component={SurveyStart} options={{headerShown: false}}/>
-      <Stack.Screen name="Survey1" component={Survey1} options={{headerShown: false}}/>
-      {/* Define other screens within the Survey Navigator if needed */}
-    </Stack.Navigator>
+    <SafeAreaView style={{flex: 1}}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SurveyStart"
+          component={SurveyStart}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey1"
+          component={Survey1}
+          options={{headerShown: false}}
+        />
+        {/* Define other screens within the Survey Navigator if needed */}
+      </Stack.Navigator>
+    </SafeAreaView>
   );
 };
 
