b02b915341d20493a0403a21b062314f3221a834
Kim Love <kimberlymlove15@gmail.com>
2024-05-02 20:43:03 -0400

    Abstracted out styles and made necessary adjustments on style props and imports

--- ConnectedToCulture/components/survey/SurveyStart.tsx
+++ ConnectedToCulture/components/survey/SurveyStart.tsx
@@ -1,28 +1,25 @@
-import {View, Text, Button, StyleSheet} from 'react-native';
-import {Dimensions} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
 import {NavigationProp} from '@react-navigation/native';
 import ResuableButton from '../reusable/reusableButton/ReusableButton';
-import globalStylesAndRowWithSpace from '../../constants/global.style';
 import COLORS from '../../constants/theme';
-import globalStyle from '../../constants/global.style';
+import surveyStyles from './survey.style';
 
 type SurveyStartProps = {
   navigation: NavigationProp<any>;
 };
 
 const SurveyStart = ({navigation}: SurveyStartProps) => {
-  const {width, height} = Dimensions.get('window');
-  const {globalStyles} = globalStylesAndRowWithSpace;
 
   return (
-    <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>Welcome to C2C survey!</Text>
-        <Text style={styles.description}>
-          Make smarter trip plans simply by answering the following 10 questions!
+    <View style={surveyStyles.container}>
+      <View style={surveyStyles.textContainer}>
+        <Text style={surveyStyles.title}>Welcome to C2C survey!</Text>
+        <Text style={surveyStyles.description}>
+          Make smarter trip plans simply by answering the following 10
+          questions!
         </Text>
-        <Text style={styles.estimatedTime}>Estimated time: 3 min</Text>
+        <Text style={surveyStyles.estimatedTime}>Estimated time: 3 min</Text>
       </View>
       <ResuableButton
         backgroundColor={COLORS.primary}
@@ -41,48 +38,3 @@ const SurveyStart = ({navigation}: SurveyStartProps) => {
 };
 
 export default SurveyStart;
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center',
-    padding: 20,
-  },
-  textContainer: {
-    alignItems: 'center',
-    marginBottom: 20,
-    width: 320
-  },
-  title: {
-    color: COLORS.primary,
-    fontFamily: 'Almarai',
-    fontSize: 24,
-    fontWeight: '700',
-    letterSpacing: 0,
-    lineHeight: 28.8,
-    paddingBottom: 0,
-  },
-  description: {
-    color: COLORS.darkGray,
-    fontFamily: 'Almarai',
-    fontSize: 16,
-    fontWeight: '400',
-    letterSpacing: 0,
-    lineHeight: 19.2,
-    marginBottom: 16,
-    paddingBottom: 0,
-    textAlign: 'center',
-  },
-  estimatedTime: {
-    color: COLORS.black,
-    fontFamily: 'Almarai',
-    fontSize: 16,
-    fontWeight: '700',
-    letterSpacing: 0,
-    lineHeight: 19.2,
-    marginBottom: 16,
-    paddingBottom: 0,
-    textAlign: 'center',
-  },
-});
