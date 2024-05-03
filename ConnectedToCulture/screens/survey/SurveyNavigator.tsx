720cae687fc7a55fb8f940e5b91dc19122e75104
Kim Love <kimberlymlove15@gmail.com>
2024-05-03 01:06:46 -0400

    Added Survey3 component to Stack.Screen

--- ConnectedToCulture/screens/survey/SurveyNavigator.tsx
+++ ConnectedToCulture/screens/survey/SurveyNavigator.tsx
@@ -4,6 +4,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import SurveyStart from '../../components/survey/SurveyStart';
 import Survey1 from '../../components/survey/Survey1';
 import Survey2 from '../../components/survey/Survey2';
+import Survey3 from '../../components/survey/Survey3';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +25,11 @@ const SurveyNavigator = () => {
         <Stack.Screen
           name="Survey2"
           component={Survey2}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey3"
+          component={Survey3}
           options={{headerShown: false}}
         />
         {/* Define other screens within the Survey Navigator if needed */}
