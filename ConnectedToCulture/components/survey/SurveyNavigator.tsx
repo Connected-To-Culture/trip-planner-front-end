5bb71bbdadf6f1e7dd4bece34e3f35e7d3317a9c
Kim Love <kimberlymlove15@gmail.com>
2024-05-07 18:13:52 -0400

    Wrapped navigation stack in survey context provider

--- ConnectedToCulture/components/survey/SurveyNavigator.tsx
+++ ConnectedToCulture/components/survey/SurveyNavigator.tsx
@@ -6,36 +6,39 @@ import SurveyStart from '../../screens/survey/SurveyStart';
 import Survey1 from '../../screens/survey/Survey1';
 import Survey2 from '../../screens/survey/Survey2';
 import Survey3 from '../../screens/survey/Survey3';
+import {SurveyDataProviderWrapper} from '../../context/surveyData';
 
 const Stack = createNativeStackNavigator();
 
 const SurveyNavigator = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SurveyStart"
-          component={SurveyStart}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Survey1"
-          component={Survey1}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Survey2"
-          component={Survey2}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Survey3"
-          component={Survey3}
-          options={{headerShown: false}}
-        />
-        {/* Define other screens within the Survey Navigator if needed */}
-      </Stack.Navigator>
-    </SafeAreaView>
+    <SurveyDataProviderWrapper>
+      <SafeAreaView style={{flex: 1}}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SurveyStart"
+            component={SurveyStart}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Survey1"
+            component={Survey1}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Survey2"
+            component={Survey2}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Survey3"
+            component={Survey3}
+            options={{headerShown: false}}
+          />
+          {/* Define other screens within the Survey Navigator if needed */}
+        </Stack.Navigator>
+      </SafeAreaView>
+    </SurveyDataProviderWrapper>
   );
 };
 
