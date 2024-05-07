2c22dd5700b1371fbc5e96e7e3a9ad03bd207f6e
Kim Love <kimberlymlove15@gmail.com>
2024-05-07 18:12:31 -0400

    Created context for the survey data selections

---
+++ ConnectedToCulture/context/surveyData.tsx
@@ -0,0 +1,96 @@
+import React, {Dispatch, SetStateAction, useState} from 'react';
+
+// Define the type for the selected state
+type SelectedState = {
+  NG: boolean;
+  ET: boolean;
+  TG: boolean;
+  ZA: boolean;
+  GH: boolean;
+  Zambia: boolean;
+  q1Text: string;
+  wildlife: boolean;
+  historical: boolean;
+  cultural: boolean;
+  adventure: boolean;
+  food: boolean;
+  entertainment: boolean;
+  q2Text: string;
+  airplane: boolean;
+  public: boolean;
+  car: boolean;
+  bicycle: boolean;
+  notSure: boolean;
+};
+
+// Define the type for the context value
+type SurveyDataContextType = {
+  selected: SelectedState;
+  setSelected: Dispatch<SetStateAction<SelectedState>>;
+}
+
+// Create the context
+const SurveyDataContext = React.createContext<SurveyDataContextType>({
+  selected: {
+    NG: false,
+    ET: false,
+    TG: false,
+    ZA: false,
+    GH: false,
+    Zambia: false,
+    q1Text: '',
+    wildlife: false,
+    historical: false,
+    cultural: false,
+    adventure: false,
+    food: false,
+    entertainment: false,
+    q2Text: '',
+    airplane: false,
+    public: false,
+    car: false,
+    bicycle: false,
+    notSure: false,
+  },
+  setSelected: () => {},
+});
+
+// Define the props type for SurveyDataProviderWrapper
+type SurveyDataProviderWrapperProps = {
+  children: React.ReactNode;
+}
+
+const SurveyDataProviderWrapper = ({
+  children,
+}: SurveyDataProviderWrapperProps) => {
+
+  const [selected, setSelected] = useState<SelectedState>({
+    NG: false,
+    ET: false,
+    TG: false,
+    ZA: false,
+    GH: false,
+    Zambia: false,
+    q1Text: '',
+    wildlife: false,
+    historical: false,
+    cultural: false,
+    adventure: false,
+    food: false,
+    entertainment: false,
+    q2Text: '',
+    airplane: false,
+    public: false,
+    car: false,
+    bicycle: false,
+    notSure: false,
+  });
+
+  return (
+    <SurveyDataContext.Provider value={{selected, setSelected}}>
+      {children}
+    </SurveyDataContext.Provider>
+  );
+}
+
+export {SurveyDataContext, SurveyDataProviderWrapper};
