7d73f87465648784fe5e1e20a26d3b1dd7d9441d
Kim Love <kimberlymlove15@gmail.com>
2024-05-07 22:58:58 -0400

    Created Progress Bar component

---
+++ ConnectedToCulture/components/survey/SurveyProgressBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+type ProgressBarProps = {
+  progress: number;
+};
+
+const ProgressBar = ({progress}: ProgressBarProps) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.progressBar, {width: `${progress}%`}]} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#CECECE',
+    borderRadius: 5,
+    height: 6,
+    marginBottom: 10,
+  },
+  progressBar: {
+    backgroundColor: '#3AA99F',
+    borderRadius: 5,
+    height: '100%',
+  },
+});
+
+export default ProgressBar;
