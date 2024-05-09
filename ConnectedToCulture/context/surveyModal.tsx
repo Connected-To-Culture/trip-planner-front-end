5dd6f868b3c961d1c6926923b2824a84318ed8cd
Kim Love <kimberlymlove15@gmail.com>
2024-05-08 22:41:13 -0400

    Made changes to surveyModal context

--- ConnectedToCulture/context/surveyModal.tsx
+++ ConnectedToCulture/context/surveyModal.tsx
@@ -3,13 +3,13 @@ import React, {Dispatch, SetStateAction, useState} from 'react';
 // Define the type for the context values
 type SurveyModalContextType = {
   modalOpen: boolean;
-  setModalOpen: Dispatch<SetStateAction<boolean>>;
+  openCloseModal: Dispatch<SetStateAction<boolean>>;
 }
 
 // Create the context
 const SurveyModalContext = React.createContext<SurveyModalContextType>({
   modalOpen: false,
-  setModalOpen: () => {},
+  openCloseModal: () => {},
 });
 
 // Define the props type for SurveyModalContextWrapper
@@ -22,8 +22,17 @@ const SurveyModalContextWrapper = ({
 }: SurveyModalContextWrapperProps) => {
   const [modalOpen, setModalOpen] = useState(false);
 
+  // const openCloseModal = (value: boolean) => setModalOpen(value);
+  const openCloseModal: SurveyModalContextType['openCloseModal'] = value => {
+    console.log('inside setter function')
+    console.log(value)
+    setModalOpen(value);
+    console.log('after value set')
+    console.log('modalOpen in setter: ', modalOpen)
+  }
+
   return (
-    <SurveyModalContext.Provider value={{modalOpen, setModalOpen}}>
+    <SurveyModalContext.Provider value={{modalOpen, openCloseModal}}>
       {children}
     </SurveyModalContext.Provider>
   );
