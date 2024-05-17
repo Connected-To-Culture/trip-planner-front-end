d168eacb1428056c527d97be4fe3448489991564
Kim Love <kimberlymlove15@gmail.com>
2024-05-16 23:59:10 -0400

    Created types file type checking when navigation using useNavigation

---
+++ ConnectedToCulture/navigation/types.ts
@@ -0,0 +1,5 @@
+export type RootStackParamList = {
+  Home: undefined; // Assuming Home doesn't take any params
+  // Add other routes here if needed
+  // Survey: { id: string }; // Example of a route with parameters
+};
