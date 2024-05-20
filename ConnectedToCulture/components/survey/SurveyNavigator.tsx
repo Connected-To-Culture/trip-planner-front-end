3b10b9f2b155a5a6095fe2e07104b42a55d61fdc
Kim Love <kimberlymlove15@gmail.com>
2024-05-20 18:07:25 -0400

    Removed SurveyDataProviderWrapper from this component

--- ConnectedToCulture/components/survey/SurveyNavigator.tsx
+++ ConnectedToCulture/components/survey/SurveyNavigator.tsx
@@ -13,78 +13,75 @@ import Survey8 from '../../screens/survey/Survey8';
 import Survey9 from '../../screens/survey/Survey9';
 import Survey10 from '../../screens/survey/Survey10';
 import SurveyEnd from '../../screens/survey/SurveyEnd';
-import {SurveyDataProviderWrapper} from '../../context/surveyData';
 
 const Stack = createNativeStackNavigator();
 
 const SurveyNavigator = () => {
   return (
-    <SurveyDataProviderWrapper>
-      <SafeAreaView style={{flex: 1}}>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="SurveyStart"
-            component={SurveyStart}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey1"
-            component={Survey1}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey2"
-            component={Survey2}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey3"
-            component={Survey3}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey4"
-            component={Survey4}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey5"
-            component={Survey5}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey6"
-            component={Survey6}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey7"
-            component={Survey7}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey8"
-            component={Survey8}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey9"
-            component={Survey9}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Survey10"
-            component={Survey10}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="SurveyEnd"
-            component={SurveyEnd}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </SafeAreaView>
-    </SurveyDataProviderWrapper>
+    <SafeAreaView style={{flex: 1}}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SurveyStart"
+          component={SurveyStart}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey1"
+          component={Survey1}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey2"
+          component={Survey2}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey3"
+          component={Survey3}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey4"
+          component={Survey4}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey5"
+          component={Survey5}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey6"
+          component={Survey6}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey7"
+          component={Survey7}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey8"
+          component={Survey8}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey9"
+          component={Survey9}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Survey10"
+          component={Survey10}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="SurveyEnd"
+          component={SurveyEnd}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </SafeAreaView>
   );
 };
 
