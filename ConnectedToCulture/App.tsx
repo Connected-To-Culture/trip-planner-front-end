af36673e0f0e845bb8830242048f2206a30b3f02
Kim Love <kimberlymlove15@gmail.com>
2024-06-10 18:01:04 -0400

    Added Toast component App and layed groundwork for customizable component

--- ConnectedToCulture/App.tsx
+++ ConnectedToCulture/App.tsx
@@ -6,28 +6,63 @@
  */
 
 import React from 'react';
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-
-
-import { CreateAccount, Welcome,Signin,TermsAndConditions,ResetPassword} from './screens';
-import { StatusBar,StyleSheet} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  CreateAccount,
+  Welcome,
+  Signin,
+  TermsAndConditions,
+  ResetPassword,
+} from './screens';
+import {StatusBar, StyleSheet} from 'react-native';
 import BottomTabNavigation from './navigation/BottomTabNavigation';
 import COLORS from './constants/theme';
 import SurveyNavigator from './components/survey/SurveyNavigator';
 import exploreCountry from './screens/exploreCountry/exploreCountry';
 import MoreExplore from './screens/explore/moreExplore/moreExplore';
 import {SurveyDataProviderWrapper} from './context/surveyData';
+import Toast, {BaseToast, ErrorToast} from 'react-native-toast-message';
+
+/*
+  1. Create the config
+*/
+const toastConfig = {
+  /*
+    Overwrite 'success' type,
+    by modifying the existing `BaseToast` component
+  */
+  success: props => (
+    <BaseToast
+      {...props}
+      style={{borderLeftColor: 'pink'}}
+      contentContainerStyle={{paddingHorizontal: 15}}
+      text1Style={{fontSize: 15, fontWeight: '400'}}
+    />
+  ),
+  /*
+    Overwrite 'error' type,
+    by modifying the existing `ErrorToast` component
+  */
+  error: props => (
+    <ErrorToast
+      {...props}
+      style={{borderLeftColor: 'red', backgroundColor: 'red'}}
+      contentContainerStyle={{paddingHorizontal: 10, paddingVertical: 20}}
+      text1Style={{fontSize: 16}}
+      text2Style={{fontSize: 15, color: 'white'}}
+    />
+  ),
+};
 
 function App(): React.JSX.Element {
- // Set the status bar background color
-StatusBar.setBackgroundColor('white');
+  // Set the status bar background color
+  StatusBar.setBackgroundColor('white');
 
-// Set the status bar style
-StatusBar.setBarStyle('dark-content');
+  // Set the status bar style
+  StatusBar.setBarStyle('dark-content');
 
-  const Stack=createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
   return (
     <SurveyDataProviderWrapper>
       <NavigationContainer>
@@ -43,10 +78,11 @@ StatusBar.setBarStyle('dark-content');
           <Stack.Screen name='more-explore' component={MoreExplore} options={{headerShown:false}} />
         </Stack.Navigator>
       </NavigationContainer>
+      <Toast />
+      {/* <Toast config={toastConfig} /> */}
     </SurveyDataProviderWrapper>
   );
 }
-
 
 export default App;
 
