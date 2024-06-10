09b35589e4d2f09476341bf191f83df0fdf4f163
Kim Love <kimberlymlove15@gmail.com>
2024-06-10 16:38:55 -0400

    Updated Plan component import in index.js

--- ConnectedToCulture/screens/index.js
+++ ConnectedToCulture/screens/index.js
@@ -5,11 +5,12 @@ import Welcome from "./authentication/Welcome";
 import Home from "./home/Home";
 import Explore from "./explore/Explore";
 import Profile from "./profile/Profile";
-import Plan from "./plan/Plan";
+// import Plan from "./plan/Plan"; old import before changes
+import Plan from "./plan/PlanMainPage";
 import TermsAndConditions from "./authentication/TermsAndConditions";
 import ResetPassword from "./authentication/ResetPassword";
 export{
-    
+
     Signin,
     CreateAccount,
     Welcome,
